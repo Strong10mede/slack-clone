f8706cfbca716f5e5e19f5578c906c86eea5b4ad
Strong10mede <kmayur809@gmail.com>
2022-10-27 20:04:36 +0530

    chat

---
+++ src/Components/Chat/Chat.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Chat.css";
+import { useParams } from "react-router-dom";
+import StarBorderOutlineIcon from "@mui/icons-material/StarBorderOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+function Chat() {
+  const { roomId } = useParams();
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <div className="chat__headerLeft">
+          <h4 className="chat__channelName">{roomId}</h4>
+        </div>
+        <div className="chat__headerRight">
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </div>
+      </div>
+      <div className="chat__messages"></div>
+    </div>
+  );
+}
+
+export default Chat;
