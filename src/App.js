a0cf50b9aab2c02fefda0826cba679bc853d9078
Strong10mede <kmayur809@gmail.com>
2022-10-27 19:06:26 +0530

    sidebar adding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
